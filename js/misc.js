7e53de80c51901ae1c8661cb550eb27c3cedf7a7
Wills Speegle <wspeegle@uga.edu>
2015-01-27 15:36:10 -0500

    TODO: add starting screen with instructions. Clean up lose/win screen general code clean up

--- js/misc.js
+++ js/misc.js
@@ -97,7 +97,7 @@ InputHandler.prototype.isPressed = function(code) {
 
 //Check if there is an intersection
 //This function is used for the collision detection between pixels
-function Intersect(ax, ay, aw, ah, bx, by, bw, bh)
+function Collision(ax, ay, aw, ah, bx, by, bw, bh)
 {
 
     if( ax < bx+bw && bx < ax+aw && ay < by+bh && by < ay+ah)
